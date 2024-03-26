8993d524336b855803f871bc88d907c7baffe7b8
SelimBHa <141486892+SelimBHa@users.noreply.github.com>
2024-03-26 18:26:58 +0100

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-
 def common_elements(set_1, set_2):
     """
     A function that returns a set of
     common elements in two sets
     """
-    return(set_1 & set_2)
+    return set_1 & set_2  # Fix the error: Add whitespace after the return keyword
