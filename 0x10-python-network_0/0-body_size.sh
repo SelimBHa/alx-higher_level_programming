cbc9f535f493da2333cb8c465641a21b617916d1
SelimBHa <141486892+SelimBHa@users.noreply.github.com>
2024-04-25 21:36:01 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-#send a GET request to the url specified and print the size of response body to stdout
-curl -sI "$1" | grep -i "Content-Length" | cut -d ":" -f 2
+
+# Check if URL argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a request to the URL and get the size of the response body
+size=$(curl -sI "$1" | grep -i '^Content-Length:' | awk '{print $2}')
+
+# Display the size of the response body in bytes
+echo "$size"
