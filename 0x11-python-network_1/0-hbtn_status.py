14940888357566303f74e51a2756a57c338cf6fe
SelimBHa <selimbenharzallah1@gmail.com>
2024-08-01 09:58:25 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""A script that
-fetches https://intranet.hbtn.io/status.
-"""
+"""A script that fetches https://intranet.hbtn.io/status."""
 
+import urllib.request
 
-if __name__ == '__main__':
-    import urllib.request
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0')
+    try:
+        with urllib.request.urlopen(req) as response:
+            content = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    except urllib.error.HTTPError as e:
+        print("HTTPError: {}".format(e.code))
+    except urllib.error.URLError as e:
+        print("URLError: {}".format(e.reason))
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        content = resp.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
